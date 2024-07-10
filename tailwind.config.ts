bffa7a192554c20a13c07964629e141738cb9e1c
Nabin Katwal <nabinkatwal2000@gmail.com>
2024-07-10 10:56:38 +0545

    husky setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["Urbanist", "sans-serif"],
+        secondary: ["Quicksand", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
